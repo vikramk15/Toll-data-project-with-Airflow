6c0d63efe5a0844dd8480509cf0f1eea21731207
vikram singh <34438006+vikramk15@users.noreply.github.com>
2024-06-19 14:19:41 +0530

    Update etl_toll_data.py

--- etl_toll_data.py
+++ etl_toll_data.py
@@ -17,41 +17,40 @@ default_dag = {
 dag = DAG('ETL_toll_data',
           schedule= timedelta(days=1),
           default_args= default_dag,
-          description="Apache Airflow Final Assignment"
           )
 
 #task to unzip data
 unzip_data = BashOperator(
     task_id= "unzip_data",
-    bash_command = "tar -xvzf /mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/tolldata.tgz",
+    bash_command = "tar -xvzf /c/Users/vikram/tolldata.tgz",
     dag = dag
 )
 
 # task to extract_data_from_csv
 extract_data_from_csv = BashOperator(
     task_id = "extract_data_from_csv",
-    bash_command = "cut -d, -f1,2,3,4 /mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/vehicle-data.csv > /mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/csv_data.csv", # -d for delimiter
+    bash_command = "cut -d, -f1,2,3,4 /c/Users/vikram/vehicle-data.csv > /c/Users/vikram/csv_data.csv", # -d for delimiter
     dag = dag
 )
 
 #task to extract data from tsv
 extract_data_from_tsv = BashOperator(
     task_id= "extract_data_from_tsv",
-    bash_command = "cut -d$'\t' -f 5,6,7 /mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/tollplaza-data.tsv > /mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/tsv_data.csv", # -d$'\t' for delimiter tab
+    bash_command = "cut -d$'\t' -f 5,6,7 /c/Users/vikram/tollplaza-data.tsv > /c/Users/vikram/tsv_data.csv", # -d$'\t' for delimiter tab
     dag = dag
 )
 
 # task to extract_data_from_fixed_width
 extract_data_from_fixed_width = BashOperator(
     task_id = "extract_data_from_fixed_width",
-    bash_command = "cut -c 59-62,63-67 /mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/payment-data.txt > /mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/fixed_width_data.csv", # -c for --characters=LIST
+    bash_command = "cut -c 59-62,63-67 /c/Users/vikram/payment-data.txt > /c/Users/vikram/fixed_width_data.csv", # -c for --characters=LIST
     dag = dag
 )
 
-csv_data = "/mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/csv_data.csv"
-tsv_data = "/mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/tsv_data.csv"
-fixed_width_data = "/mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/fixed_width_data.csv"
-extracted_data = "/mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/extracted_data.csv"
+csv_data = "/c/Users/vikram/csv_data.csv"
+tsv_data = "/c/Users/vikram/tsv_data.csv"
+fixed_width_data = "/c/Users/vikram/fixed_width_data.csv"
+extracted_data = "/c/Users/vikram/extracted_data.csv"
 # task to consolidate_data
 consolidate_data = BashOperator(
     task_id = "consolidate_data",
@@ -62,7 +61,7 @@ consolidate_data = BashOperator(
 # task to Transform and load the data
 transform_data = BashOperator(
     task_id = "transform_data", 
-    bash_command = "awk 'BEGIN {FS=OFS=\",\"} { $4= toupper($4) } 1' /mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/extracted_data.csv > /mnt/c/Users/ASUS/Documents/projects/IBM-Data-Engineer/ETL-and-Data-Pipelines-with-Shell-Airflow-and-Kafka/Project5/transformed_data.csv", 
+    bash_command = "awk 'BEGIN {FS=OFS=\",\"} { $4= toupper($4) } 1' /c/Users/vikram/extracted_data.csv > /c/Users/vikram/transformed_data.csv", 
     dag = dag
 )
 
